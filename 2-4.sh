19b10cc0bb6df4c818dd79c3b419e2283c26a8a2
Darya-Zhulitova <154079181+Darya-Zhulitova@users.noreply.github.com>
2024-03-21 11:10:34 +0300

    Update 2-4.sh

--- 2-4.sh
+++ 2-4.sh
@@ -1,63 +1,69 @@
 #!/bin/bash
 
 # Проверяем количество аргументов
-if [ $# -lt 1 ]; then
+if [ "$1" == "--help" ]; then
     echo "Необходимо указать список пользовательских групп"
-    echo "Пример для добавления: $0 group1:user1,user2 group2:user3,user4"
-    echo "Пример для удаления: $0 -d group1:user1,user2 group2:user3,user4"
+    echo "Пример для добавления: group1:user1,user2 group2:user3,user4"
+    echo "Для удаления запустите прoграмму с флагом -d"
+    echo "Пример для удаления: group1 group2"
     exit 1
 fi
 
 # Проверяем опцию -d
 if [ "$1" == "-d" ]; then
-    # Удаляем группы
+  # Удаляем группы
+  read -p "Введите через пробел список групп для удаления: " groups
+  for group in $groups
+  do
+    # Проверяем, существует ли группа
+    if grep -q "^$group:" /etc/group; then
+      # Удаляем группу
+      sudo groupdel $group
+      echo "Группа $group удалена"
+    else
+      echo "Группа $group не существует"
+      exit 1
+    fi
     shift
-    while [ $# -gt 0 ]; do
-        group=$(echo $1 | cut -d':' -f1)
-        
-        # Проверяем, существует ли группа
-	if grep -q "^$group:" /etc/group; then
-		# Удаляем группу
-		sudo groupdel $group
-		echo "Группа $group удалена"
-	else
-		echo "Группа $group не существует"
-		exit 1
-	fi
-        shift
-    done
+  done
 else
-    # Создаем группы и добавляем пользователей
-    while [ $# -gt 0 ]; do
-        group=$(echo $1 | cut -d':' -f1)
-        users=$(echo $1 | cut -d':' -f2)
-        
-        # Проверяем, существует ли группа
-	if grep -q "^$group:" /etc/group; then
-		echo "Группа $group уже существует"
-		exit 1
-	else
-		# Создаем новую группу
-		sudo groupadd $group
-		echo "Группа $group создана"
-	fi
+  # Создаем группы и добавляем пользователей
+  read -p "Введите список для добавления: " groups
+  for input in $groups
+  do
+    if ! echo $input | grep -q ":"; then
+      echo "Некорректный ввод"
+      exit 1
+    fi
+    group=$(echo $input | cut -d':' -f1)
+    users=$(echo $input | cut -d':' -f2)
 
-	# Добавляем пользователей в группу
-	for user in $(echo $users | tr ',' ' '); do
-		# Проверяем, существует ли пользователь
-		if id -u $user >/dev/null 2>&1; then
-			echo "Пользователь $user найден"	
-		else
-			# Создаем нового пользователя
-			sudo useradd $user
-			echo "Пользователь $user создан"
-		fi
-		# Добавляем пользователя в группу
-		sudo usermod -G $group $user
-		echo "Пользователь $user добавлен в группу $group"
-	done
-        shift
+    # Проверяем, существует ли группа
+    if grep -q "^$group:" /etc/group; then
+      echo "Группа $group уже существует"
+      exit 1
+    else
+      # Создаем новую группу
+      sudo groupadd $group
+      echo "Группа $group создана"
+    fi
+
+    # Добавляем пользователей в группу
+    for user in $(echo $users | tr ',' ' '); do
+      # Проверяем, существует ли пользователь
+      if id -u $user >/dev/null 2>&1; then
+        echo "Пользователь $user найден"  
+      else
+        # Создаем нового пользователя
+        sudo useradd $user
+        echo "Пользователь $user создан"
+      fi
+      # Добавляем пользователя в группу
+      sudo usermod -G $group $user
+      echo "Пользователь $user добавлен в группу $group"
     done
+    shift
+  done
 fi
 
 exit 0
